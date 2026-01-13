a6b52a757a409ba8eb96a139052d0f445e0f03b9
pop007 <yanxiaolong@looki.ai>
2026-01-13 18:39:03 +0800

    test

--- ohos/hvigorfile.ts
+++ ohos/hvigorfile.ts
@@ -1,8 +1,2 @@
-import path from 'path'
-import { appTasks } from '@ohos/hvigor-ohos-plugin';
-import { flutterHvigorPlugin } from 'flutter-hvigor-plugin';
-
-export default {
-    system: appTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
-    plugins:[flutterHvigorPlugin(path.dirname(__dirname))]         /* Custom plugin to extend the functionality of Hvigor. */
-}
+// Script for compiling build behavior. It is built in the build plug-in and cannot be modified currently.
+export { harTasks } from '@ohos/hvigor-ohos-plugin';
